42338d4de859ea8ca608a90ca16f18325919a21b
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2019-12-27 01:41:12 +0200

    Updates

---
+++ precache-manifest.a31ba1812cce6e38eccd88a7c85dc4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bf0538aac203aeabaa481f76e44b52",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "6888579da1f3a6799403",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.5a9933e8.chunk.css"
+  },
+  {
+    "revision": "9208e6ba316560a7550b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.be4b5d62.chunk.css"
+  },
+  {
+    "revision": "6888579da1f3a6799403",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.287886cc.chunk.js"
+  },
+  {
+    "revision": "9208e6ba316560a7550b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.d6a52eb6.chunk.js"
+  },
+  {
+    "revision": "7e3234e7aefa8359527f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3cf19af0.js"
+  }
+]);
